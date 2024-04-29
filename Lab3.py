cee5dfbaa956ac401f5c5bde6c8c8264a699717d
Jiehao <jiehao.23@ichat.sp.edu.sg>
2024-04-29 16:15:15 +0800

    Change to fit requirements

--- Lab3.py
+++ Lab3.py
@@ -3,44 +3,42 @@ print("Lab 3 - Software Unit Testing with PyTest")
 SORT_ASCENDING = 0
 SORT_DESCENDING = 1
 
-
 def bubble_sort(arr, sorting_order):
-
     # Copy input list to results list
     arr_result = arr.copy()
 
     # Get number of elements in the list
     n = len(arr_result)
-
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
-
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
-
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-                else:
-                    # Return an empty array
-                    arr_result = []
+    if n == 0:
+        return 0
     else:
-        arr_result = -1
+        if n < 10:
+            # Traverse through all array elements
+            for i in range(n - 1):
+                # Last i elements are already in place
+                for j in range(0, n - i - 1):
+                    # Handle non-integer elements
+                    if not isinstance(arr_result[j], int) or not isinstance(arr_result[j + 1], int):
+                        return 2
+
+                    if sorting_order == SORT_ASCENDING:
+                        if arr_result[j] > arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    elif sorting_order == SORT_DESCENDING:
+                        if arr_result[j] < arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    else:
+                        # Return an empty array
+                        return []
+
+        else:
+            return 1
 
     return arr_result
 
 def main():
     # Driver code to test above
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [64, 34, 25, 20, 22, 11, 90]
 
     # Sort in ascending order
     result = bubble_sort(arr, SORT_ASCENDING)
@@ -54,5 +52,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
